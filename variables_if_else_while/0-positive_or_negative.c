ab4380248ef4ce286d0fc3fdeaf04513d59022c8
Joseph Nishimwe <j.nishimwe@alustudent.com>
2026-01-16 19:19:31 +0200

    0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -3,8 +3,13 @@
 /* more headers goes there */
 #include <stdio.h>
 
-/* betty style doc for function main goes there */
-// this program checks for number whether positive , negative or zero
+/**
+* main - print if the number is postive, zero, or negative
+*
+* Description: using the main function
+* this program prints "Programming is positive, zero, or negative
+* Return: 0
+*/
 int main(void)
 {
 	int n;
